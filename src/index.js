c9c99b44422806d1147c636fab98f5e40edb4e88
Shally Gupta <shally_g1803@yahoo.com>
2019-09-29 00:53:42 +0530

    showing restaurants on Home page

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
-import Home from './screens/home/Home';
+import Controller from './screens/Controller';
 
 ReactDOM.render(
     <div>
-        <Header />
+        < Controller />
     </div>,
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
